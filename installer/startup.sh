41d61e3090fa83ae8cd5fca659cdf1642b453482
nadav <tashernadav@gmail.com>
2020-04-10 12:34:45 +0300

    More progress on init script

--- installer/startup.sh
+++ installer/startup.sh
@@ -12,15 +12,15 @@ docker network create -d bridge nockio-applications
 # Start containers
 docker run \
   --name nockio-dashboard \
-  --volume NOCKIO_DIRECTORY/git:/var/lib/nockio/git \
-  --volume NOCKIO_DIRECTORY/proxy:/var/lib/nockio/proxy \
+  --volume $NOCKIO_DIRECTORY/git:/var/lib/nockio/git \
+  --volume $NOCKIO_DIRECTORY/proxy:/var/lib/nockio/proxy \
   --hostname nockio-dashboard \
   --network nockio-infrastructure \
   --detach nockio-dashboard-test
 
 docker run \
   --name nockio-git \
-  --volume NOCKIO_DIRECTORY/git:/var/lib/nockio/git \
+  --volume $NOCKIO_DIRECTORY/git:/var/lib/nockio/git \
   --publish 22:22 \
   --hostname nockio-git \
   --network nockio-infrastructure \
@@ -28,7 +28,7 @@ docker run \
 
 docker run \
   --name nockio-proxy \
-  --volume NOCKIO_DIRECTORY/proxy:/var/lib/nockio/proxy \
+  --volume $NOCKIO_DIRECTORY/proxy:/var/lib/nockio/proxy \
   --publish 80:80 \
   --publish 443:443 \
   --hostname nockio-proxy \
